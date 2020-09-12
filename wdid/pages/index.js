e9dbb3b0308f3ad1b73ebe662359e958ce94baed
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-12 21:42:49 +0900

    [Update] index.js

--- wdid/pages/index.js
+++ wdid/pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head'
-
+import chrome from 'chrome'
+import axios from 'axios';
+import { useEffect } from 'react';
 export default function Home() {
+  const GetChrome = () =>{
+    console.log(chrome)
+    
+  }
+  useEffect(()=>{
+    GetChrome();
+  })
+
+  
   return (
     <div>
 
